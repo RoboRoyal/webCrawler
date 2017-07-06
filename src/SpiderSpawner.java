7d79d473016826a8ef4609457d6f6744510950f8
RoboRoyal <RoboRoyal@users.noreply.github.com>
2017-07-06 17:44:33 +0000

    Update SpiderSpawner.java
    Better way of spawning spiders

--- src/SpiderSpawner.java
+++ src/SpiderSpawner.java
@@ -1,52 +1,51 @@
-
-
+import java.util.ArrayList;
 
 public class SpiderSpawner {
 	
-	private static final String start_url = "https://bitbucket.blu";
+	//private static final String start_url = "https://bitbucket.blu";
 	
 	
 
 
 //remember headphones
 	public static void main(String[] args) throws InterruptedException{
-		int maxPages=500;
+		int maxPages=20000;
+		int numberOfThreads = 12;
+		System.out.println("Starting webcrawl");
+		if(numberOfThreads<1){
+			System.out.println("Need at least 1 thread");
+		}
 		
-		Spider spider = new Spider("num1");
-		Spider spider2 = new Spider("Num2");
-		Spider spider3 = new Spider("Num3");
-		Spider spider4 = new Spider("Num4");
-
-		spider.setMax(maxPages);
-		//spider2.setMax(65);
-		
-		
-		SpiderTamer.fileWhite(spider);
-		SpiderTamer.fileBlack(spider);
-		SpiderTamer.fileAddLinks(spider);
-		
-		spider.useBlackList(true);
-		
+		ArrayList<Spider> spiderArmy = new ArrayList<Spider>();
+		for(int x = 0;x<numberOfThreads;x++){
+			spiderArmy.add(new Spider("Spider-"+x));
+		}
+		if(spiderArmy.get(0) != null){
+			System.out.println("Adding parameters");
+			spiderArmy.get(0).setMax(maxPages);
+			SpiderTamer.fileWhite(spiderArmy.get(0));
+			SpiderTamer.fileBlack(spiderArmy.get(0));
+			SpiderTamer.fileAddLinks(spiderArmy.get(0));
+			
+		}
 		long start_time = System.currentTimeMillis();
-		//spider.startTraffic(start_url);
-		//spider2.startTraffic(start_url);
-		spider.start();
-		spider2.start();
-		spider3.start();
-		spider4.start();
-		//while(spider.getPagesVisited().size()<maxPages){
-		//	Thread.sleep(100);
-		//}
-		spider.getT().join();
-		spider2.getT().join();
-		spider3.getT().join();
-		spider4.getT().join();
+		for(Spider jock:spiderArmy){
+			System.out.println("Starting: "+jock.name);
+			jock.start();
+		}
+		for(Spider jock:spiderArmy){
+			//System.out.println("Waiting....: "+jock.getT().getName());
+			jock.getT().join();
+		}
+		
 
+		
+		
 		//System.out.println(spider.getSuccess()+" Successes and "+spider.getProblem()+" problems");
 		System.out.println("Time: "+(System.currentTimeMillis()-start_time)/1000+" seconds");
 		
 		//System.out.println(spider.getPagesVisited().toString().replaceAll(",", "\n"));
-		SpiderTamer.writeToFile(spider);
+		SpiderTamer.writeToFile(spiderArmy.get(0));
 		
 	}
 
