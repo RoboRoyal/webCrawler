e00932a5e0df172eb443a79ae5ca0d248369b7ec
RoboRoyal <RoboRoyal@users.noreply.github.com>
2017-07-06 20:50:17 +0000

    Update SpiderLeg.java

--- src/SpiderLeg.java
+++ src/SpiderLeg.java
@@ -1,23 +1,18 @@
 import java.io.IOException;
 import java.util.*;
-
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
-
-
 public class SpiderLeg {
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";//pretend we are using Internet browser
 	private List<String> links = new LinkedList<String>();
-	//private Document htmlDocument;
 	
 
 	public boolean crawl(String url) {//crawls individual web page and saves the links, returns True if successful, False otherwise
 		try{
-			Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);//conect to web page
+			Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);//Connects to web page
 			Document htmlDocument = connection.get();//get contents of web page
 			
 			if(!connection.response().contentType().contains("text/html"))//print out if connection failed
@@ -26,21 +21,18 @@ public class SpiderLeg {
                 return false;
             }
 			
-			Elements linksOnPage = htmlDocument.select(("a[href]"));//get all links from web page
+			Elements linksOnPage = htmlDocument.select("a[href]");//get all links from web page
 			for(Element link:linksOnPage){
 				this.links.add(link.absUrl("href"));//save all links in the list
 			}
 		
-		}catch(IOException e){//error handler: prints error
+		}catch(IOException e){//error handler, no need to print error, they are too common
 			return false;
 		}
 		return true;
-		
 	}
 
 	public List<String> getLinks(){//return list of new links
 		return this.links;
 	}
-
-
 }
