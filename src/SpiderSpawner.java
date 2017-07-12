5fee74542c4a6f7fcf1eab9b2f50745340a6dae4
Dakota <RoboRoyal@users.noreply.github.com>
2017-07-12 19:18:55 +0000

    Update SpiderSpawner.java

--- src/SpiderSpawner.java
+++ src/SpiderSpawner.java
@@ -1,6 +1,8 @@
 package spiders;
 
+import java.io.File;
 import java.util.ArrayList;
+import org.apache.log4j.Logger;
 
 /**
  * <h1>Java WebCrawler</h1> Crawls the web
@@ -11,60 +13,122 @@ import java.util.ArrayList;
  */
 public class SpiderSpawner {
 
-  private static final int MAX_PAGES = 100;//max number of pages to crawl (normally ends up scanning max number of pages)
-  private static final int NUMBER_OF_THREADS = 2;//Recommended between 1/10 and 1/20th of maxPages
-  private static final int PRIORITY = 5;//setting high increases performance but my lock up computer. Set between 1(lowest) and 10(max)
+	private static final int MAX_PAGES = 10;// max number of pages to crawl(normally ends up scanning max number of pages)
+	private static final int NUMBER_OF_THREADS = 2;// Recommended between 1/10 and 1/20th of maxPages		
+	private static final int PRIORITY = 5;//setting high increases performance(sometimes) but may lock up computer. Set between 1(lowest) and 10(max)
+	private static final boolean CLEAN = true;// Whether or not to clear out all written files when done
+	private static Logger logger = Logger.getLogger(SpiderSpawner.class.getCanonicalName());
 
-  private SpiderSpawner() {}
+	private SpiderSpawner() {}
 
-  @SuppressWarnings("unused")
-  public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 
-    if (NUMBER_OF_THREADS < 1) {//checks thread requirements
-      System.out.println("Need at least 1 thread");
-      System.exit(1);
-    }
-    if (PRIORITY > 10 || PRIORITY < 1) {//checks priority requirements
-      System.out.println("Priority must be set between 1 and 10");
-      System.exit(1);
-    }
-    if (MAX_PAGES < 1) {//checks pages requirements
-      System.out.println("Cant crawl less then one page");
-      System.exit(1);
-    }
+		check();//check given parameters
+		
+		ArrayList<Spider> spiderArmy = new ArrayList<Spider>();// Initializes all the spiders	
+		for (int x = 0; x < NUMBER_OF_THREADS; x++) {
+			spiderArmy.add(new Spider("Spider-" + x));
+		}
 
-    System.out.println("Starting webcrawl");
+		if (spiderArmy.get(0) != null) {// set parameters for all the threads
+			logger.info("Adding parameters...");
+			spiderArmy.get(0).setMax(MAX_PAGES);
+			Spider.doDomainSearch = false;
+			SpiderTamer.fileWhite(spiderArmy.get(0));
+			SpiderTamer.fileBlack(spiderArmy.get(0));
+			SpiderTamer.fileAddLinks(spiderArmy.get(0));
+		}
 
-    ArrayList<Spider> spiderArmy = new ArrayList<Spider>();//Initializes all the spiders
-    for (int x = 0; x < NUMBER_OF_THREADS; x++) {
-      spiderArmy.add(new Spider("Spider-" + x));
-    }
+		long startTime = System.currentTimeMillis();// used for measuring time of crawl		
+		logger.info("Starting webcrawl...");
 
-    if (spiderArmy.get(0) != null) {//set parameters for all the threads
-      System.out.println("Adding parameters");
-      spiderArmy.get(0).setMax(MAX_PAGES);
-      Spider.doDomainSearch = false;
-      SpiderTamer.fileWhite(spiderArmy.get(0));
-      SpiderTamer.fileBlack(spiderArmy.get(0));
-      SpiderTamer.fileAddLinks(spiderArmy.get(0));
+		for(int x = 0;x<spiderArmy.size();x++){//starts all the threads
+			Spider jock = spiderArmy.get(x);
+			logger.info("Starting: " + jock.name);
+			jock.start();
+			jock.getT().setPriority(PRIORITY);
+			while (Spider.pagesToVisitSize() < x+3 && Spider.pagesToVisitSize() != 0) { 
+				try {//waits for new links to crawl before starting thread
+					Thread.sleep(100);
+				} catch (Throwable e) {}
+			}
+			try {
+				Thread.sleep(10);//extra wait to allow thread to start
+			} catch (Throwable e) {}
+		}
 
-    }
-    long start_time = System.currentTimeMillis();//used for measuring time of crawl
+		while (spiderArmy.get(0).getPagesVisited().size() < MAX_PAGES && Spider.pagesToVisitSize() != 0) {
+			try {//waits until crawl is compleat or there are no pages left to crawl
+				Thread.sleep(500);
+			} catch (Throwable e) {}
+		}
+		logger.info("***Finished Crawling***");//outputs results of crawl
+		logger.info("Visited " + spiderArmy.get(0).getPagesVisited().size() + " web pages with an additional "
+				+ Spider.pagesToVisitSize() + " links found.");
+		logger.info("Downloaded: "+SpiderLeg.filesDownloaded+" files");
+		logger.info("Time: " + (System.currentTimeMillis() - startTime) / 1000 + " seconds");// prints time of crawl
+		SpiderTamer.writeToFile(spiderArmy.get(0));// save results
 
-    for (Spider jock : spiderArmy) {//starts each thread crawling
-      System.out.println("Starting: " + jock.name);
-      jock.start();
-      jock.getT().setPriority(PRIORITY);
-      try {
-        Thread.sleep(100);
-      } catch (Throwable e) {
-      }
-    }
+		logger.info("Ending any remaining threads...");
+		Spider.run = false;//tells threads to stop running
+		for (Spider jock : spiderArmy) {// waits for all threads to finish crawling
+			jock.getT().join();
+		}
+		if (CLEAN) {//Deletes all the downloaded files
+			clean();
+		}
+		logger.info("Ended successfully!");
+	}
 
-    for (Spider jock : spiderArmy) {//waits for all threads to finish crawling
-      jock.getT().join();
-    }
-    System.out.println("Time: " + (System.currentTimeMillis() - start_time) / 1000 + " seconds");//print results of crawl
-    SpiderTamer.writeToFile(spiderArmy.get(0));//save results	
-  }
+	/**
+	 * This method deletes all the imgs and docs in the output files
+	 *
+	 * @return void
+	 */
+	private static void clean() {
+		logger.info("Cleaning up files...");
+		try {
+			File folder = new File("output/imgs");//clean imgs
+			File[] files = folder.listFiles();
+			if (files != null) { // checks to make sure folder is there
+				for (File f : files) {
+					f.delete();
+				}
+			}
+			File folder2 = new File("output/files");//clean files
+			File[] files2 = folder2.listFiles();
+			if (files2 != null) { /// checks to make sure folder is there
+				for (File f : files2) {
+					f.delete();
+				}
+			}
+		} catch (Throwable e) {
+			logger.error("Problem deleting files: "+e);
+		}
+	}
+	/**
+	 * This method checks to make sure all the parameters set by user are valid
+	 *
+	 * @return void
+	 */
+	@SuppressWarnings("unused")
+	private static void check() {
+		if (NUMBER_OF_THREADS < 1) {// checks thread requirements
+			logger.error("Need at least 1 thread");
+			System.exit(1);
+		}
+		if (NUMBER_OF_THREADS > MAX_PAGES) {// checks thread requirements
+			logger.error("Can't have more threads then pages to crawl");
+			System.exit(1);
+		}
+		if (PRIORITY > 10 || PRIORITY < 1) {// checks priority requirements
+			logger.error("Priority must be set between 1 and 10");
+			System.exit(1);
+		}
+		if (MAX_PAGES < 1) {// checks pages requirements
+			logger.error("Cant crawl less then one page");
+			System.exit(1);
+		}
+	}
+	
 }
