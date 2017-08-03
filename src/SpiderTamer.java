c5f9de6afc79fd061a2330d338d2b16c19243a83
Dakota <RoboRoyal@users.noreply.github.com>
2017-08-03 21:19:51 +0000

    Update SpiderTamer.java

--- src/SpiderTamer.java
+++ src/SpiderTamer.java
@@ -11,107 +11,118 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 public class SpiderTamer {
-  private static Logger logger = Logger.getLogger(SpiderTamer.class.getCanonicalName());
+	private static Logger logger = Logger.getLogger(SpiderTamer.class.getCanonicalName());
 
-  private SpiderTamer() {}
+	private SpiderTamer() {
+	}
 
-  /**
-   * This method gets the URLs/domains from black.txt and add them to the black list
-   *
-   * @param p The spider to change
-   */
-  public static void fileBlack(Spider p) {
-    String blackFile = "Fileconfig/black.txt";
-    try (Scanner in = new Scanner(new File(blackFile))) {
-      String line;
-      while (in.hasNextLine()) {
-        line = in.nextLine();
-        if (line.length() >= 3 && !line.startsWith("#")) {//ignore lines shorter then 3 chars and lines that start with #
-          p.addBlacklistedDomain(line);
-        }
-      }
-    } catch (IOException e) {
-      logger.error("Problem getting from blacklist: " + e);
-    }
-  }
+	/**
+	 * This method gets the URLs/domains from black.txt and add them to the
+	 * black list
+	 *
+	 * @param p
+	 *            The spider to change
+	 */
+	public static void fileBlack(Spider p) {
+		String blackFile = "Fileconfig/black.txt";
+		try (Scanner in = new Scanner(new File(blackFile))) {
+			String line;
+			while (in.hasNextLine()) {
+				line = in.nextLine();
+				// ignore lines shorter then 3 chars and lines that start with #
+				if (line.length() >= 3 && !line.startsWith("#")) {
+					p.addBlacklistedDomain(line);
+				}
+			}
+		} catch (IOException e) {
+			logger.error("Problem getting from blacklist: " + e);
+		}
+	}
 
-  /**
-   * This method gets the URLs/domains from white.txt and add them to the white list
-   *
-   * @param p The spider to change
-   */
-  public static void fileWhite(Spider p) {
-    String whiteFile = "Fileconfig/white.txt";
-    try (Scanner in = new Scanner(new File(whiteFile))) {
-      String line;
-      while (in.hasNextLine()) {
-        line = in.nextLine();
-        if (line.length() >= 3 && !line.startsWith("#")) {//ignore lines shorter then 3 chars and lines that start with #
-          p.addWhitelistedDomain(line);
-        }
-      }
-    } catch (IOException e) {
-      logger.error("Problem getting from whitelist: " + e);
-    }
-  }
+	/**
+	 * This method gets the URLs/domains from white.txt and add them to the
+	 * white list
+	 *
+	 * @param p
+	 *            The spider to change
+	 */
+	public static void fileWhite(Spider p) {
+		String whiteFile = "Fileconfig/white.txt";
+		try (Scanner in = new Scanner(new File(whiteFile))) {
+			String line;
+			while (in.hasNextLine()) {
+				line = in.nextLine();
+				// ignore lines shorter then 3 chars and lines that start with #
+				if (line.length() >= 3 && !line.startsWith("#")) {
+					p.addWhitelistedDomain(line);
+				}
+			}
+		} catch (IOException e) {
+			logger.error("Problem getting from whitelist: " + e);
+		}
+	}
 
-  /**
-   * This method writes to text files the URLs crawled and the email's found
-   *
-   * @param p The spider to get info from
-   * @return boolean If the read and write were successful
-   */
-  public static boolean writeToFile(Spider p) {
-	logger.info("Writing to file...");
-    String fileOut = "output/crawledURLS.txt";
-    String mailFile = "output/foundEmails.txt";
-    Set<String> emails = new HashSet<>();
+	/**
+	 * This method writes to text files the URLs crawled and the email's found
+	 *
+	 * @param p
+	 *            The spider to get info from
+	 * @return boolean If the read and write were successful
+	 */
+	public static boolean writeToFile() {
+		logger.info("  Writing to file...");
+		String fileOut = "output/crawledURLS.txt";
+		String mailFile = "output/foundEmails.txt";
+		Set<String> emails = new HashSet<>();
+		// Separates out all the emails
+		for (String line : Spider.getPagesVisited()) {
+			if (line.contains("mailto")) {
+				emails.add(line);
+			}
+		}
+		try (Writer spiderJocky = new BufferedWriter(new FileWriter(new File(fileOut)));
+				Writer spiderJocky2 = new BufferedWriter(new FileWriter(new File(mailFile)))) {
 
-    for (String line : p.getPagesVisited()) {//Separates out all the emails
-      if (line.contains("mailto")) {
-        emails.add(line);
-      }
-    }
-    try (Writer spiderJocky = new BufferedWriter(new FileWriter(new File(fileOut)));
-        Writer spiderJocky2 = new BufferedWriter(new FileWriter(new File(mailFile)))) {
+			// write crawledURLS
+			logger.info("  Attempting to write " + Spider.getPagesVisited().size() + " links to file...");
+			spiderJocky.write(Spider.getPagesVisited().toString().replaceAll(",", "\n").replaceAll("mailto.*", " ")
+					.replaceFirst("\\]", " ").replaceFirst("\\[", " "));
 
-      //write crawledURLS
-      logger.info("Attempting to write " + p.getPagesVisited().size() + " links to file...");
-      spiderJocky.write(p.getPagesVisited().toString().replaceAll(",", "\n").replaceAll("mailto.*", " ")
-          .replaceFirst("\\]", " ").replaceFirst("\\[", " "));
+			// write emailsFound
+			int emailCount = emails.size();
+			if (emailCount > 0) {
+				logger.info("  Attempting to write " + emailCount + " emails to file...");
+				spiderJocky2.write(
+						emails.toString().replaceFirst("\\]", " ").replaceAll(",", "\n").replaceFirst("\\[", " "));
+			}
+		} catch (IOException e) {
+			logger.error("Problem writing to file: " + e.getMessage(), e);
+			return false;
+		}
+		logger.info("  Written!");
+		return true;
+	}
 
-      //write emailsFound
-      int emailCount =  emails.size();
-      if(emailCount > 0){
-          logger.info("Attempting to write " + emailCount + " emails to file...");
-          spiderJocky2.write(emails.toString().replaceFirst("\\]", " ").replaceAll(",", "\n")
-              .replaceFirst("\\[", " "));
-      }
-    } catch (IOException e) {
-      logger.error("Problem writing to file: " + e.getMessage(), e);
-      return false;
-    }
-    logger.info("Written!");
-    return true;
-  }
-
-  /**
-   * This method gets the URLs from linksToCrawl and adds them to the list of links to be crawled
-   *
-   * @param p The spider to add links to
-   */
-  public static void fileAddLinks(Spider p) {
-    String urlFile = "Fileconfig/linksToCrawl.txt";
-    try (Scanner in = new Scanner(new File(urlFile))) {
-      String line;
-      while (in.hasNextLine()) {
-        line = in.nextLine();
-        if (line.length() >= 3 && !line.startsWith("#")) {//ignore lines shorter then 3 chars and lines that start with #
-          p.addURL(line);
-        }
-      } 
-    } catch (IOException e) {
-      logger.error("Problem reading from '" + urlFile + ": " + e);
-    }
-  }
+	/**
+	 * This method gets the URLs from linksToCrawl and adds them to the list of
+	 * links to be crawled
+	 *
+	 * @param p
+	 *            The spider to add links to
+	 */
+	public static void fileAddLinks() {
+		String urlFile = "Fileconfig/linksToCrawl.txt";
+		try (Scanner in = new Scanner(new File(urlFile))) {
+			String line;
+			while (in.hasNextLine()) {
+				line = in.nextLine();
+				// ignore lines shorter then 3 chars and lines that start with #
+				if (line.length() >= 3 && !line.startsWith("#")) {
+					Spider.addURL(line);
+				}
+			}
+		} catch (IOException e) {
+			logger.error("Problem reading from '" + urlFile + ": " + e);
+		}
+	}
 }
