5e42556f8c957a33073b052e5d30bfadd7fd426a
Dakota <RoboRoyal@users.noreply.github.com>
2017-09-05 23:32:24 -0400

    Update SpiderLeg.java

--- src/SpiderLeg.java
+++ src/SpiderLeg.java
@@ -1,7 +1,6 @@
 package spiders;
 
 import java.io.BufferedOutputStream;
-import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
